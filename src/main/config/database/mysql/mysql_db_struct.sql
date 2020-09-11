af6ec559c98e2011e05968d93269b01e907732db
iulian <iuliancl2000@yahoo.com>
2020-09-11 15:20:36 +0300

    add Facebook related columns to users table

--- src/main/config/database/mysql/mysql_db_struct.sql
+++ src/main/config/database/mysql/mysql_db_struct.sql
@@ -311,6 +311,9 @@ CREATE TABLE jforum_users (
   user_reputation DOUBLE,
   user_authhash VARCHAR(32),
   user_twitter VARCHAR(50) DEFAULT NULL,
+  fb_access_token VARCHAR(255) DEFAULT NULL,
+  fb_refresh_token VARCHAR(255) DEFAULT NULL,
+  fb_token_expire BIGINT DEFAULT 0,
   PRIMARY KEY (user_id)
 ) ENGINE=InnoDB;
 
